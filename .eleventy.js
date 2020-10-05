4b74bdf91df942e42c54b0d38ad9a890704d0c6f
Dave Wolfe <dave@red58.com>
2020-10-04 17:56:08 -0700

    Initial commit (LHB static)

---
+++ .eleventy.js
@@ -0,0 +1,24 @@
+const md = require('markdown-it')();
+
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy('src/content/fonts');
+  eleventyConfig.addPassthroughCopy('src/content/images');
+
+  eleventyConfig.addWatchTarget('src/styles/style.css');
+  eleventyConfig.addPassthroughCopy({'src/styles':'./styles'});
+
+  eleventyConfig.addWatchTarget('src/scripts/*.js');
+  eleventyConfig.addPassthroughCopy({'src/scripts':'./scripts'});
+
+  eleventyConfig.addFilter('markdown', function(value) {
+    if (typeof value === 'string') {
+      return module.render(value);
+    }
+
+    return value;
+  });
+
+  return {
+    dir: { input: 'src/content', output: 'dist' }
+  };
+};
