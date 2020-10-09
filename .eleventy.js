892d8fe10b6c270f8a71bd832467e5cdcbdad37c
Dave Wolfe <dave@red58.com>
2020-10-08 17:03:31 -0700

    Minify HTML on build

--- .eleventy.js
+++ .eleventy.js
@@ -2,14 +2,13 @@ const md = require('markdown-it')();
 const { minify } = require("terser");
 
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy('src/content/fonts');
-  eleventyConfig.addPassthroughCopy('src/content/images');
+  eleventyConfig.addPassthroughCopy('./src/content/images');
 
-  eleventyConfig.addWatchTarget('src/styles/style.css');
-  eleventyConfig.addPassthroughCopy({ 'src/styles': './styles' });
+  eleventyConfig.addWatchTarget('./src/styles/style.css');
+  eleventyConfig.addPassthroughCopy({ './src/styles': './styles' });
 
-  eleventyConfig.addWatchTarget('src/scripts/*.js');
-  eleventyConfig.addPassthroughCopy({ 'src/scripts': './scripts' });
+  eleventyConfig.addWatchTarget('./src/scripts/*.js');
+  eleventyConfig.addPassthroughCopy({ './src/scripts': './scripts' });
 
   eleventyConfig.addFilter('markdown', function (value) {
     if (typeof value === 'string') {
@@ -18,6 +17,8 @@ module.exports = function (eleventyConfig) {
 
     return value;
   });
+
+  eleventyConfig.addTransform("htmlmin", require("./src/utils/minify-html.js"));
 
   eleventyConfig.addNunjucksAsyncFilter("jsmin", async function (
     code,
@@ -35,6 +36,6 @@ module.exports = function (eleventyConfig) {
 
   return {
     pathPrefix: '/lhb-static/',
-    dir: { input: 'src/content', output: 'dist' }
+    dir: { input: './src/content', output: 'dist' }
   };
 };
